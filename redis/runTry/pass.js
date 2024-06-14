637805a32d6828b6d81cc1a1d4f0777fed0ea6a5
Syuq <tatd.wine@gmail.com>
2024-06-14 15:02:46 +0700

    🚀 feat(bcrypt): Hash

---
+++ redis/runTry/pass.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+  try {
+    // Generate a salt with 128-bit strength
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    console.log(`::salt:`, salt)
+
+    // Hash the password using the generated salt
+    const hash = await bcrypt.hash(password, salt);
+
+    // Store the hashed password in your database
+    console.log('Hashed password', hash);
+    
+    // At this point, you can store the hash in your database
+    // When verifying password, you'll compare the stored hash with the newly generated hash using bcrypt,compare()
+
+  } catch (error) {
+    console.error("Error hashing password:", error);
+  }
+}
+
+
+// Call the async function
+const password = 'abc123';
+hashPassword(password);
