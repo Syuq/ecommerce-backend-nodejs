9ca0ad88faed8f3514a72d00b40fa21b1859ab5b
Syuq <tatd.wine@gmail.com>
2025-02-15 00:02:32 +0700

    🚀 feat(app): initialize Express application with middleware

---
+++ src/app.js
@@ -0,0 +1,46 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const {default:helmet} = require("helmet")
+const compression = require("compression")
+const app = express()
+
+// init middlewares
+app.use(morgan("dev"))
+//morgan("combined") for product
+//morgan("common")
+//morgan("short")
+//morgan("tiny")
+//morgan("dev") for dev
+app.use(helmet())
+app.use(compression()) // offload
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+// test pub/sub redis 
+require('./test/inventory.test')
+const productTest =require('./test/product.test')
+productTest.purchaseProduct('product:001',10)
+// init db
+require('./dbs/init.mongodb')
+//const {checkOverload} = require('./helpers/check.connect')
+//checkOverload();
+// init route
+app.use('/',require('./routes'))
+// handling error
+app.use((req,res,next)=>{
+    const error = new Error('Not found')
+    error.status=404
+    next(error)
+})
+app.use((error,req,res,next)=>{
+    const statusCode =error.status || 500
+    return res.status(statusCode).json({
+        status:'error',
+        code:statusCode,
+        message:error.message || 'Internal server error'
+    })
+})
+
+module.exports = app
