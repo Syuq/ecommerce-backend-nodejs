7487a9635d85382322cac170892ad7601058f0a2
Syuq <tatd.wine@gmail.com>
2025-02-15 00:08:40 +0700

    🚀 feat(database): add MongoDB initialization script

---
+++ src/dbs/init.mongodb.js
@@ -0,0 +1,37 @@
+const  mongoose  = require("mongoose")
+const {db:{host,port,name}} = require("../configs/config.mongodb")
+const connectString =`mongodb://${host}:${port}/${name}`
+const {countConnect} = require('../helpers/check.connect')
+
+class Database {
+    constructor(){
+        this.connect()
+    }
+    // connect 
+     connect(type ='mongodb'){
+        if(1 === 1 )
+        {
+            mongoose.set('debug',true)
+            mongoose.set('debug',{color:true})
+        }
+         mongoose.connect(connectString,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(_ =>{
+            console.log("Connected MongoDb ",countConnect())
+        })
+        .catch(err => console.log(`error`))
+
+    }
+    static getInstance()
+    {
+        if(!Database.instance)
+        {
+            Database.instance = new Database()
+        }
+        return Database.instance
+        
+    }
+}
+const instanceMongodb=Database.getInstance();
+module.exports =instanceMongodb
