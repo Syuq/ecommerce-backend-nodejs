94786929f78979d0633e73973dd24e25b9ce9bcb
Syuq <tatd.wine@gmail.com>
2025-02-16 18:22:46 +0700

    🚀 feat(comment): add comment routes with authentication

---
+++ src/routes/comment/index.js
@@ -0,0 +1,15 @@
+const express =require('express')
+const CommentController =require('../../controller/comment.controller')
+const route= express.Router();
+const asyncHandler = require('../../helpers/asyncHandler');
+const { authenticationV2 } = require('../../auth/authUtils');
+
+//authentication///
+route.use(authenticationV2)
+///////////
+
+route.post('',asyncHandler(CommentController.createComment))
+route.delete('',asyncHandler(CommentController.deleteComment))
+route.get('',asyncHandler(CommentController.getCommentByParentId))
+
+module.exports=route
