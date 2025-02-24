31d6db368a6b753ab1dce1ecf2a5f643015bb8ae
Syuq <tatd.wine@gmail.com>
2025-02-24 22:42:17 +0700

    style(rabbitmq): add missing semicolons for consistency

--- sys_message_queue_shopDEV/src/dbs/init.rabbit.js
+++ sys_message_queue_shopDEV/src/dbs/init.rabbit.js
@@ -1,56 +1,60 @@
-'use strict'
+'use strict';
 
-const amqp = require('amqplib')
+const amqp = require('amqplib');
 
 const connectToRabbitMQ = async () => {
   try {
-    const connection = await amqp.connect('amqp://localhost')
-    if(!connection) throw new Error(' Connection not established')
+    const connection = await amqp.connect('amqp://guest:123456@localhost');
+    if (!connection) throw new Error(' Connection not established');
 
-    const channel = await connection.createChannel()
+    const channel = await connection.createChannel();
 
-    const queue = 'sys_message_queue_shopDEV'
-    await channel.assertQueue(queue, { durable: false })
-    return {channel, connection}
+    const queue = 'sys_message_queue_shopDEV';
+    await channel.assertQueue(queue, { durable: false });
+    return { channel, connection };
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 const connectToRabbitMQForTest = async () => {
   try {
-    const {channel, connection} = await connectToRabbitMQ()
+    const { channel, connection } = await connectToRabbitMQ();
 
-    const queue = 'test_queue'
-    const message = 'Hello, shopDEV by Syuq'
+    const queue = 'test_queue';
+    const message = 'Hello, shopDEV by Syuq';
 
-    await channel.assertQueue(queue)
-    await channel.sendToQueue(queue, Buffer.from(message))
+    await channel.assertQueue(queue);
+    await channel.sendToQueue(queue, Buffer.from(message));
 
     // close the connection
-    await connection.close()
+    await connection.close();
   } catch (error) {
-    console.log(`Error connection to RabbitMQ`, error)
+    console.log(`Error connection to RabbitMQ`, error);
   }
-}
+};
 
-const consumerQueue = async ( channel, queueName ) => {
+const consumerQueue = async (channel, queueName) => {
   try {
-    await channel.assertQueue(queueName, { durable: true })
-    console.log(`Waiting for messages...`)
-    channel.consume( queueName, msg => {
-      console.log(`Received message: ${queueName}::`, msg.content.toString())
-    }, {
-      noAck: true
-    })
+    await channel.assertQueue(queueName, { durable: true });
+    console.log(`Waiting for messages...`);
+    channel.consume(
+      queueName,
+      (msg) => {
+        console.log(`Received message: ${queueName}::`, msg.content.toString());
+      },
+      {
+        noAck: true
+      }
+    );
   } catch (error) {
-    console.error(`error publish message to rabbitMQ::`, error)
-    throw error
+    console.error(`error publish message to rabbitMQ::`, error);
+    throw error;
   }
-}
+};
 
 module.exports = {
   connectToRabbitMQ,
   connectToRabbitMQForTest,
   consumerQueue
-}
+};
