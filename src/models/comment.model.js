137fe72495aebcca1eb646f49d7797e9b7e50d7b
Syuq <tatd.wine@gmail.com>
2025-04-13 23:07:34 +0700

    refactor: cleanup controller logic & unify naming for product publishing
    - Refactored product controller to use consistent formatting and arrow functions.
    - Standardized naming from 'PublishProductByshop' to 'PublishProductByShop'.
    - Improved formatting and comments in comment.model.js with schema JSDoc.
    - Updated repository methods to match naming and behavior conventions.
    
    - Ensures naming consistency across layers (controller, service, repo).
    - Improves maintainability and readability.
    - Prepares for future automation, documentation, or validation tooling.

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,18 +1,36 @@
-const {model,Schema} = require("mongoose")
-const DOCUMENT_NAME = 'Comments'
-const COLLECTION_NAME = 'Comments'
+'use strict';
 
-const commentSchema = new Schema({
-    comment_productId :{type:Schema.Types.ObjectId,ref:'Products'},
-    comment_userId :{type:Number,default:1},
-    comment_content :{type:String,default:'text'},
-    comment_left :{type:Number,default:0},
-    comment_right:{type:Number,default:0},
-    comment_parentId:{type:Schema.Types.ObjectId,ref:DOCUMENT_NAME},
-    isDeleted:{type:Boolean,default:false}
-},{
-    timestamps:true,
-    collection:COLLECTION_NAME
-})
+const { model, Schema } = require('mongoose');
+const DOCUMENT_NAME = 'Comments';
+const COLLECTION_NAME = 'Comments';
 
-module.exports = model(DOCUMENT_NAME,commentSchema)
+/**
+ * Mongoose schema definition for Comments, representing user comments on products
+ * with nested comment support and tracking metadata.
+ *
+ * @schema commentSchema
+ * @property {ObjectId} comment_productId - Reference to the associated product
+ * @property {Number} comment_userId - User ID of the comment author
+ * @property {String} comment_content - Text content of the comment
+ * @property {Number} comment_left - Left boundary for nested comment tracking
+ * @property {Number} comment_right - Right boundary for nested comment tracking
+ * @property {ObjectId} comment_parentId - Reference to parent comment for nested comments
+ * @property {Boolean} isDeleted - Soft delete flag for the comment
+ */
+const commentSchema = new Schema(
+  {
+    comment_productId: { type: Schema.Types.ObjectId, ref: 'Products' },
+    comment_userId: { type: Number, default: 1 },
+    comment_content: { type: String, default: 'text' },
+    comment_left: { type: Number, default: 0 },
+    comment_right: { type: Number, default: 0 },
+    comment_parentId: { type: Schema.Types.ObjectId, ref: DOCUMENT_NAME },
+    isDeleted: { type: Boolean, default: false }
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME
+  }
+);
+
+module.exports = model(DOCUMENT_NAME, commentSchema);
--- src/models/repositories/product.repo.js
+++ src/models/repositories/product.repo.js
@@ -1,118 +1,111 @@
-const {product,electronic,furniture,clothing} = require("../../models/product.model")
-const {Types} = require("mongoose")
-const {getSelectData,getUnSelectData, convertToObjectIdMongoDb} = require("../../utils/index")
-const { findByIdAndUpdate } = require("../keytoken.model")
-const findAllDraftForShop = async({query,limit,skip})=>{
-    return await queryProduct({query,limit,skip})
+const { product, electronic, furniture, clothing } = require('../../models/product.model');
+const { Types } = require('mongoose');
+const { getSelectData, getUnSelectData, convertToObjectIdMongoDb } = require('../../utils/index');
+const { findByIdAndUpdate } = require('../keytoken.model');
+const findAllDraftForShop = async ({ query, limit, skip }) => {
+  return await queryProduct({ query, limit, skip });
+};
+const findAllPublishForShop = async ({ query, limit, skip }) => {
+  return await queryProduct({ query, limit, skip });
+};
+const searchProductByUser = async ({ keySearch }) => {
+  const regexSearch = new RegExp(keySearch);
+  const results = await product
+    .find(
+      {
+        isPublish: true,
+        $text: { $search: regexSearch }
+      },
+      { score: { $meta: 'textScore' } }
+    )
+    .sort({ score: { $meta: 'textScore' } })
+    .lean();
+  return results;
+};
 
-}
-const findAllPublishForShop = async({query,limit,skip})=>{
-    return await queryProduct({query,limit,skip})
+const PublishProductByShop = async ({ product_shop, product_id }) => {
+  const foundShop = await product.findOne({
+    product_shop: new Types.ObjectId(product_shop),
+    _id: new Types.ObjectId(product_id)
+  });
+  if (!foundShop) {
+    return null;
+  }
 
-}
-const searchProductByuser = async ({keySearch})=>{
-    const regexSearch = new RegExp(keySearch)
-    const results = await product.find({
-        isPublish:true,
-        $text:{$search:regexSearch}},
-        {score:{$meta:'textScore'}})
-        .sort({score:{$meta:'textScore'}}).lean()
-    return results
-}
+  foundShop.isDraft = false;
+  foundShop.isPublish = true;
+  const { modifiedCount } = await foundShop.updateOne(foundShop);
+  return modifiedCount;
+};
+const unPublishProductByShop = async ({ product_shop, product_id }) => {
+  const foundShop = await product.findOne({
+    product_shop: new Types.ObjectId(product_shop),
+    _id: new Types.ObjectId(product_id)
+  });
+  if (!foundShop) {
+    return null;
+  }
 
-const PublishProductByshop = async({product_shop,product_id})=>{
-    const foundShop = await product.findOne({
-        product_shop: new Types.ObjectId(product_shop),
-        _id: new Types.ObjectId(product_id)
-    })
-    if(!foundShop)
-    {
-        return null
-    }
+  foundShop.isDraft = true;
+  foundShop.isPublish = false;
+  const { modifiedCount } = await foundShop.updateOne(foundShop);
+  return modifiedCount;
+};
 
-    foundShop.isDraft = false
-    foundShop.isPublish = true
-    const {modifiedCount} = await foundShop.updateOne(foundShop)
-    return modifiedCount
-}
-const unPublishProductByshop = async({product_shop,product_id})=>{
-    const foundShop = await product.findOne({
-        product_shop: new Types.ObjectId(product_shop),
-        _id: new Types.ObjectId(product_id)
-    })
-    if(!foundShop)
-    {
-        return null
-    }
+const findAllProduct = async ({ limit, sort, page, filter, select }) => {
+  const skip = (page - 1) * limit;
+  const sortBy = sort === 'ctime' ? { _id: -1 } : { _id: 1 };
+  const products = await product.find(filter).sort(sortBy).skip(skip).limit(limit).select(getSelectData(select)).lean();
 
-    foundShop.isDraft = true
-    foundShop.isPublish = false
-    const {modifiedCount} = await foundShop.updateOne(foundShop)
-    return modifiedCount
-}
+  return products;
+};
 
-const findAllProduct = async ({limit ,sort,page,filter,select})=>{
-    const skip =(page-1)*limit
-    const sortBy = sort === 'ctime' ?{_id:-1}:{_id:1}
-    const products = await product.find(filter)
-    .sort(sortBy)
-    .skip(skip)
-    .limit(limit)
-    .select(getSelectData(select))
-    .lean()
-    
-    return products
-}
-
-const queryProduct =async({query,limit ,skip})=>{
-    return await product.find(query)
-    .populate('product_shop','name email -_id')
-    .sort({updateAt:-1})
+const queryProduct = async ({ query, limit, skip }) => {
+  return await product
+    .find(query)
+    .populate('product_shop', 'name email -_id')
+    .sort({ updateAt: -1 })
     .skip(skip)
     .limit(limit)
     .lean()
-    .exec()
-}
-const findProduct = async ({product_id,unSelect})=>{
-    return await product.findById(product_id).select(getUnSelectData(unSelect))
-}
+    .exec();
+};
+const findProduct = async ({ product_id, unSelect }) => {
+  return await product.findById(product_id).select(getUnSelectData(unSelect));
+};
 
-const updateProductById = async({
-    productId,
-    bodyUpdate,
-    model,
-    isNew = true
-})=>{
-    return await model.findByIdAndUpdate(productId,bodyUpdate,{new:isNew})
-}
+const updateProductById = async ({ productId, bodyUpdate, model, isNew = true }) => {
+  return await model.findByIdAndUpdate(productId, bodyUpdate, { new: isNew });
+};
 
-const getProductByid = async(productId)=>{
-    return await product.findOne({_id:convertToObjectIdMongoDb(productId)}).lean()
-}
+const getProductById = async (productId) => {
+  return await product.findOne({ _id: convertToObjectIdMongoDb(productId) }).lean();
+};
 
-const checkProductBySever = async(products)=>{
-    return await Promise.all(products.map(async product =>{
-        const foundProduct = await getProductByid(product.productId)
-        if(foundProduct)
-        {
-            return {
-                price:foundProduct.product_price,
-                quantity:product.quantity,
-                productId:product.productId
-            }
-        }
-    }))
-}
+const checkProductBySever = async (products) => {
+  return await Promise.all(
+    products.map(async (product) => {
+      const foundProduct = await getProductById(product.productId);
+      if (foundProduct) {
+        return {
+          price: foundProduct.product_price,
+          quantity: product.quantity,
+          productId: product.productId
+        };
+      }
+    })
+  );
+};
 
-module.exports={
-    findAllDraftForShop,
-    PublishProductByshop,
-    findAllPublishForShop,
-    unPublishProductByshop,
-    searchProductByuser,
-    findAllProduct,
-    findProduct,
-    updateProductById,
-    getProductByid,
-    checkProductBySever
-}
+module.exports = {
+  findAllDraftForShop,
+  PublishProductByShop,
+  findAllPublishForShop,
+  unPublishProductByShop,
+  searchProductByUser,
+  findAllProduct,
+  findProduct,
+  updateProductById,
+  getProductById,
+  checkProductBySever
+};
