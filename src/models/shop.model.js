34a35f7d9f9d574880c2746fae1c8a53bfdb383a
Syuq <tatd.wine@gmail.com>
2025-04-15 17:37:00 +0700

    chore(shop.model): reformat schema definition and clean up code

--- src/models/shop.model.js
+++ src/models/shop.model.js
@@ -1,41 +1,43 @@
-const {model,Schema,Types} = require('mongoose'); // Erase if already required
-const DOCUMENT_NAME='Shop'
-const COLLECTION_NAME ='Shops'
+const { model, Schema, Types } = require('mongoose'); // Erase if already required
+const DOCUMENT_NAME = 'Shop';
+const COLLECTION_NAME = 'Shops';
 // Declare the Schema of the Mongo model
-//!dmbgum
-var shopSchema = new Schema({
-    name:{
-        type:String,
-        trim:true,
-        maxLength:150,
+
+var shopSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      maxLength: 150
     },
-    email:{
-        type:String,
-        unique:true,
-        trim:true,
+    email: {
+      type: String,
+      unique: true,
+      trim: true
     },
-    status:{
-        type:String,
-        required:true,
-        enum:['active','inactive'],
-        default:'inactive'
+    status: {
+      type: String,
+      required: true,
+      enum: ['active', 'inactive'],
+      default: 'inactive'
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true
     },
-    verify:{
-        type:Schema.Types.Boolean,
-        default:false
+    verify: {
+      type: Schema.Types.Boolean,
+      default: false
     },
-    roles:{
-        type:Array,
-        default:[]
+    roles: {
+      type: Array,
+      default: []
     }
-},{
-    timestamps:true,
-    collection:COLLECTION_NAME
-}
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME
+  }
 );
 
 //Export the model
